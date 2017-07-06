4e2b4efea04a26527364cd82eafcaefa42d039ff
Steve Roudebush <roudy@umich.edu>
2017-07-05 22:58:53 -0500

    initial set of plugins

---
+++ install_win.ps1
@@ -0,0 +1,15 @@
+﻿$nvimConfigDir = "$HOME\AppData\Local\nvim\"
+$nvimAutoloadDir = $nvimConfigDir + "autoload"
+$nvimBundleDir = $nvimConfigDir + "bundle"
+$pathogenFilePath = $nvimAutoloadDir + "\pathogen.vim"
+
+if ((Test-Path $nvimAutoloadDir) -eq $false) {
+    New-Item $nvimAutoloadDir -ItemType directory
+}
+
+if ((Test-Path $nvimBundleDir) -eq $false) {
+    New-Item $nvimBundleDir -ItemType directory
+}
+
+$downloadCommand = "Invoke-WebRequest -Uri `"https://tpo.pe/pathogen.vim`" -Outfile `"$pathogenFilePath`""
+iex $downloadCommand
